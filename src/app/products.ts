44851651110647dba596fcfb1817c020228a8b0f
Leonardo <leonardo.castellanos@sofka.com.co>
2019-08-05 15:40:01 -0500

    Eliminacion de comentarios

--- src/app/products.ts
+++ src/app/products.ts
@@ -20,10 +20,3 @@ export const products = [
     description: 'Its a basic phone'
   }
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
