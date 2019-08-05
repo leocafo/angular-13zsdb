0e41fbdb3f06d6dea48935b8103d921ea3c7caa0
Leonardo <leonardo.castellanos@sofka.com.co>
2019-08-05 15:17:46 -0500

    Adicion de productos

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,23 @@
 export const products = [
   {
     name: 'Phone XL',
-    price: 799,
+    price: '$ 799',
     description: 'A large phone with one of the best screens'
   },
   {
     name: 'Phone Mini',
-    price: 699,
+    price: '$ 699',
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standard',
-    price: 299,
-    description: ''
+    price: '$ 299',
+    description: 'Its a phone standard'
+  },
+  {
+    name: 'Phone Basic',
+    price: '$ 99',
+    description: 'Its a basic phone'
   }
 ];
 
